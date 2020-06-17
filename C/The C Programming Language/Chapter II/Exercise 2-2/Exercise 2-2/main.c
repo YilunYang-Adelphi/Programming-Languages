ea94d643da9594cfecfd3c5725d5f95133cdb733
Yilun Yang <yilunyang@mail.adelphi.edu>
2020-06-17 00:12:12 -0400

    Create main.c

---
+++ C/The C Programming Language/Chapter II/Exercise 2-2/Exercise 2-2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Exercise 2-2
+//
+//  Created by Yilun Yang on 6/17/20.
+//  Copyright © 2020 Yilun Yang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
