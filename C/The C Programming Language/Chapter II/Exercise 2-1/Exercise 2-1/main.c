cb096c339f04525a4f70ce231e710255eb50df38
Yilun Yang <yilunyang@mail.adelphi.edu>
2020-06-16 23:47:11 -0400

    Create main.c

---
+++ C/The C Programming Language/Chapter II/Exercise 2-1/Exercise 2-1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Exercise 2-1
+//
+//  Created by Yilun Yang on 6/16/20.
+//  Copyright © 2020 Yilun Yang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("variable's ranges: \n");
+    return 0;
+}
