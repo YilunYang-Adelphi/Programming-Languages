da3f1b5c0013391aeda6e9e16752ce4c01ae6db3
Yilun Yang <yilunyang@mail.adelphi.edu>
2020-06-30 01:22:53 -0400

    Create main.c

---
+++ C/The C Programming Language/Chapter II/Exercise 2-5/Exercise 2-5/main.c
@@ -0,0 +1,50 @@
+//
+//  main.c
+//  Exercise 2-5
+//
+//  Created by Yilun Yang on 6/30/20.
+//  Copyright © 2020 Yilun Yang. All rights reserved.
+//
+#include <stdio.h>
+#define MAX 100
+int any(char s[], char t[]);
+
+int main(int argc, const char * argv[]) {
+    char s[MAX], t[MAX], c;
+    
+    printf("Input a char set here:\n");
+    int i = 0;
+    while ((c = getchar()) != '\n') {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    
+    printf("Input another char set here:\n");
+    i = 0;
+    while ((c = getchar()) != '\n') {
+        t[i++] = c;
+    }
+    t[i] = '\0';
+    
+    printf("The first char set is:\n%s\n",s);
+    printf("The second char set is:\n%s\n",t);
+    
+    printf("squeeze:\n%d\n",any(s, t));
+    
+    return 0;
+}
+
+int any(char s[], char t[])
+{
+    int i = 0, j = 0;
+    while (s[i] != '\0') {
+        j = 0;
+        while (t[j] != '\0') {
+            if (t[j++] == s[i]) {
+                return i;
+            }
+        }
+        i++;
+    }
+    return -1;
+}
